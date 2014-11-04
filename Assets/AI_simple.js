b28f85873aeee0bfc4bcded032d25ccbad0501ae
AnotherFoxGuy <haselnut116601@gmail.com>
2014-11-04 11:59:23 +0100

    Added the game

---
+++ Assets/AI_simple.js
@@ -0,0 +1,63 @@
+//UnnamedGameProject
+
+public var MoveTo : Transform;
+public var idleAnimation : AnimationClip;
+public var walkAnimation : AnimationClip;
+public var runAnimation : AnimationClip;
+public var jumpPoseAnimation : AnimationClip;
+
+public var stoppingDistance : float;
+
+
+
+private var CurrText = "";
+
+private var Used = true;
+
+
+
+private var agent: NavMeshAgent;
+
+
+function Start () {
+	agent = GetComponent.<NavMeshAgent>();
+	agent.stoppingDistance = stoppingDistance;
+}
+
+function Update () {
+
+	agent.SetDestination(MoveTo.position);
+
+
+	if(agent.remainingDistance >= 999999999999999999){
+			CurrText = "I cant see you";
+			//agent.Stop();
+			//animation.Play(idleAnimation.name);
+	}
+	else{
+			CurrText = "I can see you";
+		}
+	CurrText = CurrText+ " @ "+agent.remainingDistance;
+	if (agent.remainingDistance >= stoppingDistance){
+		animation.Play(walkAnimation.name);
+
+	}
+	else{
+		animation.Play(idleAnimation.name);
+	}
+
+}
+
+
+function OnGUI () {
+    GUI.Label (Rect (25, 25, 100, 30), CurrText);
+}
+
+function OnTriggerEnter () {
+	//Used = false;
+	//agent.Stop();
+}
+
+function OnTriggerExit () {
+	//Used = true;
+}
---
+++ Assets/testscript.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+// JavaScript
+private var selectedToolbar : int = 0;
+private var toolbarStrings = ["One", "Two"];
+
+function OnGUI () {
+    // Determine which button is active, whether it was clicked this frame or not
+    selectedToolbar = GUI.Toolbar (Rect (50, 10, Screen.width - 100, 30), selectedToolbar, toolbarStrings);
+
+    // If the user clicked a new Toolbar button this frame, we'll process their input
+    if (GUI.changed)
+    {
+        print ("The toolbar was clicked");
+
+        if (selectedToolbar == 0)
+        {
+            print ("First button was clicked");
+        }
+        else
+        {
+            print ("Second button was clicked");
+        }
+    }
+}
