220417a86c97cc4cacb73967fc462f6c23b84155
AnotherFoxGuy <haselnut116601@gmail.com>
2014-11-06 15:22:32 +0100

    Updated the game

--- Assets/Scripts/ScoreScript.js
+++ Assets/Scripts/ScoreScript.js
@@ -1,20 +1,56 @@
 ﻿#pragma strict
-/*
-private var Score = 0;
-function OnGUI () {
-  //if (ShowButton){
-    // Make a background box
-    GUI.Box (Rect (10,10,100,90), "Loader Menu");
 
-    // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-    //if (GUI.Button (Rect (200,400,800,200), "Go to ?")) {
-      //Application.LoadLevel (LoadScene);
-  //  }
-  //}
+private var KeysFound = 0;
+private var ShowTextBox = false;
+
+function OnGUI () {
+  GUI.Box (Rect (Screen.width - 250,100,200,100), "Key found "+KeysFound);
+  if(ShowTextBox){
+    GUI.Box (Rect (Screen.width/2,Screen.height/2,100,50), " Key ");
+  }
 }
 
-function OnCollisionEnter(OtherObj : Collision) {
-  if(OtherObj.name == "This")
-		Destroy (OtherObj.gameObject);
+function Update(){
+  var ray = Camera.main.ScreenPointToRay (Vector3(Screen.width/2,Screen.width/2, 0));
+  var hit : RaycastHit;
+  //var otherObj :
+  if (Physics.Raycast (ray, hit, 5))
+  //Debug.DrawLine (ray.origin, hit.point);
+    {
+    if(hit.transform.tag == "Key"){
+      //print("Hit key");
+      ShowTextBox = true;
+    }
+  }
+  else{
+    ShowTextBox = false;
+  }
+
+if(ShowTextBox && Input.GetButtonDown("Submit")){
+    if(hit.transform.tag == "Key"){
+    print("Found a key");
+    Destroy (hit.collider.gameObject);
+    KeysFound++;
+  }
+}
+
+
+}
+/*
+function OnTriggerEnter(otherObj: Collider) {
+  print("col @ "+otherObj);
+
+    if (otherObj.transform.tag == "Key") {
+        Destroy (otherObj.gameObject);
+        KeysFound++;
+    }
+}
+
+function Update() {
+	var ray = Camera.main.ScreenPointToRay (Vector3(Screen.width/2,Screen.width/2, 0));
+	var hit : RaycastHit;
+	if (Physics.Raycast (ray, hit, 10)) {
+		Debug.DrawLine (ray.origin, hit.point);
+	}
 }
 */
