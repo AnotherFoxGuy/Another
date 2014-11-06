94cf83970490b3afc955f71ccc4672bdf2e0024e
AnotherFoxGuy <haselnut116601@gmail.com>
2014-11-06 11:42:41 +0100

    Updated the game

---
+++ Assets/Scripts/SceneSwitch.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+public var LoadScene : String;
+public var ButtonText : String;
+
+private var ShowButton = false;
+
+@script RequireComponent (Collider)
+
+
+function OnTriggerEnter(){
+  ShowButton = true;
+
+}
+function OnTriggerExit(){
+  ShowButton = false;
+
+}
+
+function OnGUI () {
+  if (ShowButton){
+    // Make a background box
+    //GUI.Box (Rect (10,10,100,90), "Loader Menu");
+
+    // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+    if (GUI.Button (Rect (Screen.width/3,Screen.height/3,Screen.width/3,Screen.height/3), ButtonText)) {
+        Application.LoadLevel (LoadScene);
+    }
+  }
+}
---
+++ Assets/Scripts/ScoreScript.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+/*
+private var Score = 0;
+function OnGUI () {
+  //if (ShowButton){
+    // Make a background box
+    GUI.Box (Rect (10,10,100,90), "Loader Menu");
+
+    // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+    //if (GUI.Button (Rect (200,400,800,200), "Go to ?")) {
+      //Application.LoadLevel (LoadScene);
+  //  }
+  //}
+}
+
+function OnCollisionEnter(OtherObj : Collision) {
+  if(OtherObj.name == "This")
+		Destroy (OtherObj.gameObject);
+}
+*/
