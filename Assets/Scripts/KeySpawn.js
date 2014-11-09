0520de12dab7fd690b0e919734e50ec78ceef0da
AnotherFoxGuy <haselnut116601@gmail.com>
2014-11-09 19:47:55 +0100

    Updated the game

---
+++ Assets/Scripts/KeySpawn.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+public var prefab : GameObject;
+
+function Start () {
+  SpawnKey();
+  SpawnKey();
+  SpawnKey();
+  SpawnKey();
+  SpawnKey();
+}
+
+function SpawnKey(){
+  var position = Vector3(Random.Range(-20.0, 20.0), Random.Range(2.0, 10.0), Random.Range(-20.0, 20.0));
+  Instantiate(prefab, position, Quaternion.identity);
+}
--- Assets/Scripts/SceneSwitch.js
+++ Assets/Scripts/SceneSwitch.js
@@ -4,13 +4,18 @@ public var LoadScene : String;
 public var TextBoxText : String;
 
 private var ShowTextBox = false;
+private var CanLoadLevel = false;
 
 @script RequireComponent (Collider)
 
 
 function OnTriggerEnter(){
   ShowTextBox = true;
-
+  var gos : GameObject[];
+  gos = GameObject.FindGameObjectsWithTag("Key");
+  if (gos.length == 0) {
+      CanLoadLevel = true;
+  }
 }
 function OnTriggerExit(){
   ShowTextBox = false;
@@ -19,12 +24,19 @@ function OnTriggerExit(){
 
 function OnGUI () {
   if (ShowTextBox){
+    if (!CanLoadLevel){
+    GUI.Box (Rect (Screen.width/2,Screen.height/2,200,150), "You haven't found all keys yet!");
+    }
+    else{
     GUI.Box (Rect (Screen.width/2,Screen.height/2,200,150), TextBoxText);
+    }
   }
 }
 function Update(){
-  if(ShowTextBox && Input.GetButtonDown("Submit")){
+  if (CanLoadLevel){
+    if(ShowTextBox && Input.GetButtonDown("Submit")){
       print("LoadLevel "+LoadScene);
       Application.LoadLevel (LoadScene);
     }
+  }
 }
